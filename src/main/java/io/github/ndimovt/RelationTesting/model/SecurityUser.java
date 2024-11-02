1a87a69060a415b0c61e2b1a1e3fa0b471981afa
ndimovt <nikolaitodorov119@gmail.com>
2024-11-02 16:31:58 +0200

    adde expiration time and secret key to application.propertires, created JwtService.java in service package, implemented extractClaim(), extractUsername(), generateToken(), override generateToken(), getJwtExpiration(), isTokenValid(), isTokenExpired(), extractExpiration(), buildToken(), extractAllClaims(), getSignInKey() in JwtService.java, getAuthorities in SecurityUser.java returns empty List

--- src/main/java/io/github/ndimovt/RelationTesting/model/SecurityUser.java
+++ src/main/java/io/github/ndimovt/RelationTesting/model/SecurityUser.java
@@ -15,7 +15,7 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(user::getAuthority);
+        return List.of();
     }
 
     @Override
